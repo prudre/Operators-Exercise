1a38c6afc484f9d0b1f09c8f9acdaa0c16adb9c0
Andrae Vang <andrae.vang@gmail.com>
2024-05-10 17:04:04 -0700

    operators exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Exercise 1
+            int a = 17;
+
+            int b = 4;
+
+            int sum = a + b;
+
+            int subtraction = a - b;
+
+            int multiply = a * b;
+
+            int quotient = a / b;
+
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+           
         }
     }
 }
